5aaf1a458ecfb5a8776651262c45bb6085864feb
mwhittaker1 <mickeywhittaker@gmail.com>
2025-04-12 17:16:29 -0400

    Working lambda code, not working with 9xxxx zip codes? what?

--- backend/lambdaMain.py
+++ backend/lambdaMain.py
@@ -8,18 +8,20 @@ root_url = "https://api.openweathermap.org/data/3.0/onecall?"
 c = "current"
 
 #gets location from state/zipcode
-def get_location(state, zipcode):
+def get_location(zipcode):
+   country_code = "US"
    print("Getting location...")
    location_data = requests.get(
-       f"https://api.openweathermap.org/geo/1.0/zip?zip={zipcode},{state}&appid={API_KEY}"
+       f"http://api.openweathermap.org/geo/1.0/zip?zip={zipcode},{country_code}&appid={API_KEY}"
    ).json()
+   
    lat = location_data["lat"]
    lon = location_data["lon"]
-   return lat, lon
-
+   loc_name = location_data["name"]
+   return lat, lon, loc_name
 
 #pulls data from url
-def get_weather(zip: str, lat, lon):
+def get_weather(lat, lon):
    print("Getting weather...")
    exclude = "hourly,daily,minutely"
    url = f"{root_url}lat={lat}&lon={lon}&exclude={exclude}&appid={API_KEY}".strip(
@@ -29,7 +31,6 @@ def get_weather(zip: str, lat, lon):
    #print(f"Data fetched! {data}")
    return data
 
-
 #get time from data
 def weather_time(weather_info):
    print("Getting time...")
@@ -37,8 +38,12 @@ def weather_time(weather_info):
    utc_time = datetime.datetime.fromtimestamp(time)
    est = pytz.timezone('US/Eastern')
    time = utc_time.astimezone(est)
-   return weather_info, time
-
+   stime = str(time)
+   year = stime[:4]
+   date = stime[5:10]
+   hourmin = stime[11:16]
+   time_message =  f"It is currently {hourmin} on {date}-{year}"
+   return time_message
 
 #get temp from data
 def weather_temp(weather_info):
@@ -47,13 +52,11 @@ def weather_temp(weather_info):
    temp = round((temp - 273.15) * 1.8 + 32)
    return weather_info, temp
 
-
 #get humidity from data
 def weather_humidity(weather_info):
    print("Getting humidity...")
    humidity = weather_info[c]['humidity']
    return weather_info, humidity
-
 
 #get cloud percent from data
 def weather_cloudPercent(weather_info):
@@ -61,51 +64,48 @@ def weather_cloudPercent(weather_info):
    cloudPercent = weather_info[c]['clouds']
    return weather_info, cloudPercent
 
-
 #format time, temp, humidity, and cloud percent into strings
-def format_weather(weather_info, time, temp, humidity, cloudPercent):
+def format_weather(temp, humidity, cloudPercent):
    print("Formatting weather...")
-   str_time = "time: " + str(time) + " | "
    str_temp = "temp: " + str(temp) + " | "
    str_humidity = "humidity: " + str(humidity) + " | "
    str_cloudPercent = "Cloud Cover Percent: " + str(cloudPercent) + " | "
-   return str_time, str_temp, str_humidity, str_cloudPercent
-
+   return str_temp, str_humidity, str_cloudPercent
 
 def hardcode_location():
    lon = -74.949203
    lat = 39.857819
    return lon, lat
 
-
-def build_message(str_time, str_temp, str_humidity, str_cloudPercent):
+def build_message(datetime, str_temp, str_humidity, str_cloudPercent, loc_name):
    print("Building message...")
    b = "\n"
-   welcome = f"{b}{b}Welcome here's the weather for Voorhees, NJ!{b}"
-   message = welcome + b + str_time + b + b + str_temp + b + b + str_humidity + b + b + str_cloudPercent
+   welcome = f"{b}{b}Welcome here's the weather for {loc_name}{b}"
+   message = welcome + b + datetime + b + b + str_temp + b + b + str_humidity + b + b + str_cloudPercent
+   print(message)
    return message
 
-
 #main function to run all functions
-
 def lambda_handler(event, context):
    print("Running main...")
-   
    zip_code = None
    if event.get("queryStringParameters") and "zip" in event["queryStringParameters"]:
       zip_code = event["queryStringParameters"]["zip"]
-
-   
+   else:
+      return {
+            "statusCode": 400,
+            "body": "Missing or invalid 'zip' parameter"
+        }
    #lat, lon = get_location("NJ", "08043")
-   lon, lat = hardcode_location()
-   weather_info = get_weather(context, lat, lon)
-   weather_info, time = weather_time(weather_info)
+   lon, lat, loc_name = get_location(zip_code) #calls event for input of zip, returns lon, lat of location
+   weather_info = get_weather(lat, lon) 
+   datetime = weather_time(weather_info)
    weather_info, temp = weather_temp(weather_info)
    weather_info, humidity = weather_humidity(weather_info)
    weather_info, cloudPercent = weather_cloudPercent(weather_info)
-   str_time, str_temp, str_humidity, str_cloudPercent = format_weather(
-       weather_info, time, temp, humidity, cloudPercent)
-   message = build_message(str_time, str_temp, str_humidity, str_cloudPercent)
+   str_temp, str_humidity, str_cloudPercent = format_weather(
+       temp, humidity, cloudPercent)
+   message = build_message(datetime, str_temp, str_humidity, str_cloudPercent, loc_name)
    print("Message built!")
    print(message)
    return {
